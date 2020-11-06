a051036ba7ff35fc7fb7d935967e24cd25877082
Eric Stratton <strattonericj@gmail.com>
2020-11-06 08:33:01 -0800

    introduce prototype to add size to Pizza object

---
+++ README.md
@@ -0,0 +1,5 @@
+## Specs
+
+| Test | Input | Output |
+| :------------| :---------------| :-----------|
+| Take size from user input and add it to their order | S, M, L, Xl | "Size: <S, M, L, Xl>"
---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Business Logic //
+
+function Pizza() {
+  this.toppings = [];
+  this.size = "";
+}
+
+Pizza.prototype.addSize = function() {
+  this.size.push();
+}
+
+
+function Toppings() {
+  this.artichoke = artichoke;
+  this.blackOlives = blackOlives;
+  this.tomatoes = tomatoes;
+  this.pepperoncini = pepperoncini;
+  this.kalamata = kalamata;
+  this.balsamic = balsamic;
+}
